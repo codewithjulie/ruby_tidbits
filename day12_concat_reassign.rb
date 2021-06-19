693324486a3cac674999f71c2a809984134866c1
codewithjulie <codewithjulie@gmail.com>
2021-06-19 16:10:43 -0700

    day 12 concat vs reassignment

--- day12_concat_reassign.rb
+++ day12_concat_reassign.rb
@@ -16,35 +16,3 @@ name1 += "by"   # name1 gets reassigned, the same as name1 = name1 + "by"
 puts name1      # Bobby
 puts name2      # Bob
 puts name1.equal?(name2)  # false, they no longer point to the same object
-
-
-
-
-
-
-
-
-# p bob.equal?(bob2)
-
-# bob += "baz"
-# p bob, bob.object_id
-# p bob2, bob2.object_id
-
-# arr1 = ["red", "orange"]
-# arr2 = arr1
-
-# arr1.concat(["yellow"])
-
-# p arr1, arr1.object_id
-# p arr2, arr2.object_id
-
-# arr1 << "green"
-
-# p arr1, arr1.object_id
-# p arr2, arr2.object_id
-
-# arr1 += ["blue"]
-
-# p arr1, arr1.object_id
-# p arr2, arr2.object_id
-
